5571c8dbe1cde24090284abf7339d395a4a153c0
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-08-25 13:58:26 +0530

    feat: initialize shadcn/ui

--- frontend/src/components/LanguageSelector.jsx
+++ frontend/src/components/LanguageSelector.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const languages = [
   { code: "en", lng: "English" },
-  { code: "si", lng: "Sinhala" },
+  { code: "si", lng: "සිංහල" },
 ];
 
 const LanguageSelector = () => {
@@ -15,19 +24,26 @@ const LanguageSelector = () => {
 
   return (
     <div className="flex gap-2">
-      {languages.map((lng) => {
-        return (
-          <button
-            className={`py-1 px-3 bg-accent rounded-md ${
-              lng.code === i18n.language ? "bg-primary text-white" : ""
-            }`}
-            key={lng.code}
-            onClick={() => changeLanguage(lng.code)}
-          >
-            {lng.lng}
-          </button>
-        );
-      })}
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white border-none flex flex-col gap-1">
+          <DropdownMenuLabel>Languages</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {languages.map((lng) => {
+            return (
+              <DropdownMenuItem
+                className={`cursor-pointer rounded-md ${
+                  lng.code === i18n.language ? "bg-primary text-white" : ""
+                }`}
+                key={lng.code}
+                onClick={() => changeLanguage(lng.code)}
+              >
+                {lng.lng}
+              </DropdownMenuItem>
+            );
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  prefix: "",
   theme: {
     colors: {
       primary: "#333333",
@@ -12,7 +18,22 @@ export default {
       border: "#cccccc",
       black: "#222222",
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
