75f2017e37a8325aeaea273a78d0c1fd2e1d78b0
Hasitha <hasithapeiris.me@gmail.com>
2025-06-15 14:59:38 +0530

    fix: fix i18n configuration

--- frontend/src/utils/i18n/i18n.js
+++ frontend/src/utils/i18n/i18n.js
@@ -1,21 +1,26 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import enLang from "./locales/en/home.json";
-import siLang from "./locales/si/home.json";
+import enHome from "./locales/en/home.json";
+import siHome from "./locales/si/home.json";
 
 const resources = {
   en: {
-    translation: enLang,
+    home: enHome,
   },
   si: {
-    translation: siLang,
+    home: siHome,
   },
 };
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources,
-  debug: true,
-  fallbackLng: "en",
-  returnObjects: true,
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    debug: false,
+    fallbackLng: "en",
+    defaultNS: "home",
+    ns: ["home"],
+    returnObjects: true,
+  });
